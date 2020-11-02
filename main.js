3d3756e59a43c1511b568257e073c579f89587ed
Simon <sim.borkowski@gmail.com>
2020-11-02 22:56:23 +0100

    Updated version.html and package.json

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // This is free and unencumbered software released into the public domain.
 // See LICENSE for details
 
-const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
+const {app, BrowserWindow, Menu, protocol, ipcMain, MenuItem, shell, dialog} = require('electron');
 const log = require('electron-log');
 const {autoUpdater} = require("electron-updater");
 
@@ -59,8 +59,8 @@ function sendStatusToWindow(text) {
   win.webContents.send('message', text);
 }
 function createDefaultWindow() {
-  win = new BrowserWindow();
-  win.webContents.openDevTools();
+  win = new BrowserWindow({ width: 400, height: 320});
+  //win.webContents.openDevTools();
   win.on('closed', () => {
     win = null;
   });
@@ -92,6 +92,7 @@ app.on('ready', function() {
   // Create the Menu
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
+  menu.append(MenuItem())
 
   createDefaultWindow();
 });
---
+++ start.ps1
@@ -0,0 +1,3 @@
+[Environment]::SetEnvironmentVariable("GH_TOKEN","277130870b51dc6a7a95b58f8172bfeb8799b315","User")
+npm run publish
+pause
